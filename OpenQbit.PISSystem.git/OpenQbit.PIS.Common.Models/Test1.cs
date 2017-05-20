d8631a4e031fec98974195cd38d13744a17ebe28
Sandun <sandunssw@gmail.com>
2017-05-20 16:08:07 +0530

    Test class added

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.Common.Models/Test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.PIS.Common.Models
+{
+    public class Test1
+    {
+
+    }
+}
