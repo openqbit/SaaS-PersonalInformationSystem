82379877bbd88ab08c105f6bbf61dbc09a129aaf
LahiruAbenayaka <lakmalahiru44@gmail.com>
2017-06-03 13:32:54 +0530

    dal project aded

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.DataAccess.DAL/PISContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using OpenQbit.PIS.Common.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace OpenQbit.PIS.DataAccess.DAL
+{
+    public class PISContext :DbContext
+    {
+        public PISContext() : base("PISDB")
+        {
+            Configuration.ProxyCreationEnabled = false;
+        }
+        public DbSet<Message> Message { get; set; }
+        public DbSet <MessageDetail> MessageDetail { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Notification> Notification { get; set; }
+
+        public DbSet<TaskDetail> TaskDetail { get; set; }
+        public DbSet<Common.Models.Task> Task { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
