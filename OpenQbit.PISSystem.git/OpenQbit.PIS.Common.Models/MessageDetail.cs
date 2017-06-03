a8f14759b2e65de75ec135d3f7d096245b9fedd7
LahiruAbenayaka <lakmalahiru44@gmail.com>
2017-06-03 11:19:05 +0530

    Employee clz deleted

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.Common.Models/MessageDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.PIS.Common.Models
+{
+    public class MessageDetail
+    {
+        public int ID { get; set; }
+        public bool StatusE_mail{ get; set; }
+        public bool StatusInternalE_mail { get; set; }
+        public bool StatusSMS { get; set; }
+        public int EmployeeID { get; set; }
+        public int MessageID { get; set; }
+
+        public virtual Message Message { get; set; }
+        public virtual Employee Employee { get; set; }
+
+    }
+}
