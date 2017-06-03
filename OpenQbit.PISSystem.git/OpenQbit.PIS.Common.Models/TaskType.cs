a751a4fe4568d0f87c2cb109764afd04bcf03692
Sandun <sandunssw@gmail.com>
2017-06-03 12:19:27 +0530

    created class TaskType

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.Common.Models/TaskType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.PIS.Common.Models
+{
+    public class TaskType
+    {
+        private string ID { get; set; }
+        private string TaskName { get; set; }
+        private ICollection<TaskDetail> TaskDetail { get; set; }
+    }
+}
