9628c52ee6172ea6542c4f2ad05139aeed059cf3
sandun <sandunssw@gmail.com>
2017-06-16 15:25:07 +0530

    services project and employee api model

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.Service.WebAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace OpenQbit.PIS.Service.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
