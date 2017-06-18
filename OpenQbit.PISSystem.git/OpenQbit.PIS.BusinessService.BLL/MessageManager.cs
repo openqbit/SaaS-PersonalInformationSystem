92c2f0bd78a9ad453100bc0161e2f259e00a4857
LahiruAbenayaka <lakmalahiru44@gmail.com>
2017-06-18 20:30:38 +0530

    IMessageManger and IServiceCommon and Message Mannger Clz Created

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.BusinessService.BLL/MessageManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.PIS.BusinessService.Contracts;
+
+
+namespace OpenQbit.PIS.BusinessService.BLL
+{
+    public class MessageManager : IMessageMannager
+    {
+        IRepository _repository;
+        public bool Delete<T>(T obj) where T : class
+        {
+            return _repository.Delete(obj);
+        }
+
+        public T Find<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return _repository.Find(predicate);
+        }
+
+        public List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return _repository.FindList(predicate);
+        }
+
+        public List<T> GetAll<T>() where T : class
+        {
+            return _repository.GetAll<T>();
+        }
+
+        public bool Recored<T>(T obj) where T : class
+        {
+            return _repository.Create(obj);
+        }
+
+        public bool Save()
+        {
+            return _repository.Save();
+        }
+
+        public bool Update<T>(T obj) where T : class
+        {
+            return _repository.Update(obj);
+        }
+    }
+}
---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.BusinessService.Contracts/IMessageMannager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.PIS.BusinessService.Contracts
+{
+    public interface IMessageMannager : ICommonServiceManager 
+    {
+
+    }
+}
