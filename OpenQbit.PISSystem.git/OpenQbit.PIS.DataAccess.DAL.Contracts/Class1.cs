c9c8a977cc4b1dc2b954e1b0d16d8b8f481aa958
sandunssw <sandunssw@gmail.com>
2017-06-19 22:35:42 +0530

    create project DAL.contracts and create class INotificationManager(Bizservice.Contracts)

--- OpenQbit.PISSystem.git/OpenQbit.PIS.DataAccess.DAL.Contracts/Class1.cs
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.DataAccess.DAL.Contracts/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenQbit.PIS.BusinessService.Contracts
+namespace OpenQbit.PIS.DataAccess.DAL.Contracts
 {
     public class Class1
     {
