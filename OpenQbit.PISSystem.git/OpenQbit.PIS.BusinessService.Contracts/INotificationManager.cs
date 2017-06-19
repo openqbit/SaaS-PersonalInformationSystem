70a8d5e58b39d471e9c4589a1301729ba8f39f91
sandunssw <sandunssw@gmail.com>
2017-06-19 23:13:47 +0530

    create INotificationmgr

--- OpenQbit.PISSystem.git/OpenQbit.PIS.BusinessService.Contracts/INotificationManager.cs
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.BusinessService.Contracts/INotificationManager.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenQbit.PIS.DataAccess.DAL.Contracts
+namespace OpenQbit.PIS.BusinessService.Contracts
 {
-    public class Class1
+    public interface INotificationManager : ICommonServiceManager
     {
+
     }
 }
