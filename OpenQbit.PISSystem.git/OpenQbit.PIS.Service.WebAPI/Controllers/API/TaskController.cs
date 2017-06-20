e869e0b3c9e75de67a38806ce939dc556d7e9bce
Naduni <coolnadunitharushika@gmail.com>
2017-06-20 12:15:00 +0530

    created APITaskModel and Task Controller in web.API

---
+++ OpenQbit.PISSystem.git/OpenQbit.PIS.Service.WebAPI/Controllers/API/TaskController.cs
@@ -0,0 +1,52 @@
+﻿using OpenQbit.PIS.BusinessService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using OpenQbit.PIS.Service.WebAPI;
+using OpenQbit.PIS.Service.WebAPI.Models.API;
+using System.Net;
+
+namespace OpenQbit.PIS.Service.WebAPI.Controllers.API
+{
+    public class TaskController : ApiController
+    {
+        ITaskManager _taskManager = UnityResolver.Resolve<ITaskManager>();
+
+        public HttpResponseMessage Post(APITaskModel document)
+        {
+            if (_taskManager.Recored(document)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        public HttpResponseMessage Put(APITaskModel document)
+        {
+            if (_taskManager.Update(document)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        public HttpResponseMessage Delete(APITaskModel document)
+        {
+            if (_taskManager.Delete(document)) return new HttpResponseMessage(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        public APITaskModel Get(int? ID)
+        {
+            
+
+            return _taskManager.Find<APITaskModel>(e => e.ID == ID);
+        }
+
+        public List<APITaskModel> GetAll()
+        {
+            
+            return _taskManager.GetAll<APITaskModel>();
+        }
+    }
+}
+        
+    }
+}
